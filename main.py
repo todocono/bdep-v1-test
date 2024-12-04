569c4bc41e02a653f08dcfb0a8edfe2e0daca2eb
Rodolfo <todocono@gmail.com>
2024-12-04 13:19:31 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,98 @@
+def readSensors():
+    global pull0, pull1, sensorState
+    if input.pin_is_pressed(TouchPin.P0):
+        pull0 = 0
+    else:
+        pull0 = 1
+    if input.pin_is_pressed(TouchPin.P1):
+        pull1 = 0
+    else:
+        pull1 = 1
+    sensorState = pull0 + pull1 * 2
+
+def on_received_number(receivedNumber):
+    global codeFromRemote
+    codeFromRemote = receivedNumber
+    updateVibration()
+    datalogger.log(datalogger.create_cv("event", 2),
+        datalogger.create_cv("signalStrength",
+            abs(radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH))),
+        datalogger.create_cv("sensorState", sensorState),
+        datalogger.create_cv("codeFromRemote", codeFromRemote),
+        datalogger.create_cv("temp", input.temperature()),
+        datalogger.create_cv("accel",
+            max(input.acceleration(Dimension.X),
+                max(input.acceleration(Dimension.Y),
+                    input.acceleration(Dimension.Z)))))
+radio.on_received_number(on_received_number)
+
+# 8G is way too much, but this safety feature needs testing in a real backpack
+
+def on_gesture_eight_g():
+    pass
+input.on_gesture(Gesture.EIGHT_G, on_gesture_eight_g)
+
+def on_log_full():
+    datalogger.log(datalogger.create_cv("event", 99))
+    datalogger.delete_log(datalogger.DeleteType.FAST)
+datalogger.on_log_full(on_log_full)
+
+def updateVibration():
+    global signalReceived
+    if codeFromRemote == 3:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+        pins.digital_write_pin(DigitalPin.P12, 1)
+        music.ring_tone(165)
+        led.plot(1, 0)
+        led.plot(1, 1)
+        signalReceived = control.millis()
+    elif codeFromRemote == 1:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+        pins.digital_write_pin(DigitalPin.P12, 0)
+        music.ring_tone(262)
+        led.plot(1, 0)
+        led.unplot(1, 1)
+        signalReceived = control.millis()
+    elif codeFromRemote == 2:
+        pins.digital_write_pin(DigitalPin.P12, 1)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        music.ring_tone(494)
+        led.unplot(1, 0)
+        led.plot(1, 1)
+        signalReceived = control.millis()
+
 def on_button_pressed_a():
     pins.digital_write_pin(DigitalPin.P8, 1)
-    basic.show_number(2)
+    basic.show_number(1)
     pins.digital_write_pin(DigitalPin.P8, 0)
     basic.clear_screen()
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def init():
+    global pull0, pull1, codeFromRemote, sensorState
+    music.set_built_in_speaker_enabled(True)
+    music.set_volume(255)
+    pins.set_pull(DigitalPin.P0, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P1, PinPullMode.PULL_UP)
+    pull0 = 0
+    pull1 = 0
+    basic.clear_screen()
+    radio.set_group(1)
+    radio.set_transmit_power(7)
+    radio.set_frequency_band(0)
+    codeFromRemote = 0
+    sensorState = 0
+    datalogger.delete_log(datalogger.DeleteType.FAST)
+    datalogger.include_timestamp(FlashLogTimeStampFormat.MILLISECONDS)
+    serial.redirect_to_usb()
+    datalogger.mirror_to_serial(True)
+    datalogger.set_column_titles("event",
+        "signalStrength",
+        "sensorState",
+        "codeFromRemote",
+        "temp",
+        "accel")
+    datalogger.log(datalogger.create_cv("event", 1))
 
 def on_button_pressed_b():
     pins.digital_write_pin(DigitalPin.P12, 1)
@@ -12,27 +101,102 @@ def on_button_pressed_b():
     basic.clear_screen()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-music.play(music.string_playable("G - B - - F F C5 ", 240),
-    music.PlaybackMode.UNTIL_DONE)
-pins.set_pull(DigitalPin.P0, PinPullMode.PULL_UP)
-pins.set_pull(DigitalPin.P1, PinPullMode.PULL_UP)
-pins.digital_write_pin(DigitalPin.P8, 1)
-pins.digital_write_pin(DigitalPin.P12, 1)
-basic.show_icon(IconNames.HEART)
-basic.show_icon(IconNames.SMALL_HEART)
-pins.digital_write_pin(DigitalPin.P8, 0)
-pins.digital_write_pin(DigitalPin.P12, 0)
-basic.clear_screen()
+def FeedbackMyself():
+    global signalReceived, sensorState
+    if sensorState == 3:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+        pins.digital_write_pin(DigitalPin.P12, 1)
+        music.ring_tone(165)
+        radio.send_number(3)
+        led.plot(0, 0)
+        led.plot(0, 1)
+        signalReceived = control.millis()
+    elif sensorState == 1:
+        pins.digital_write_pin(DigitalPin.P8, 1)
+        music.ring_tone(262)
+        radio.send_number(2)
+        pins.digital_write_pin(DigitalPin.P12, 0)
+        led.plot(0, 0)
+        led.unplot(0, 1)
+        signalReceived = control.millis()
+    elif sensorState == 2:
+        pins.digital_write_pin(DigitalPin.P12, 1)
+        music.ring_tone(494)
+        radio.send_number(1)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        led.unplot(0, 0)
+        led.plot(0, 1)
+        signalReceived = control.millis()
+    elif (0) == (6):
+        radio.send_number(6)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        pins.digital_write_pin(DigitalPin.P12, 0)
+        music.stop_all_sounds()
+        led.unplot(0, 0)
+        led.unplot(0, 1)
+        sensorState = 0
+def timeoutVibration():
+    global codeFromRemote
+    if control.millis() - signalReceived >= 100:
+        music._play_default_background(music.built_in_playable_melody(Melodies.DADADADUM),
+            music.PlaybackMode.IN_BACKGROUND)
+        pins.digital_write_pin(DigitalPin.P8, 0)
+        pins.digital_write_pin(DigitalPin.P12, 0)
+        music.stop_all_sounds()
+        led.unplot(1, 0)
+        led.unplot(1, 1)
+        codeFromRemote = 0
+def welcome():
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    music.play(music.tone_playable(131, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    music.play(music.tone_playable(659, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
+        music.PlaybackMode.UNTIL_DONE)
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    music.play(music.tone_playable(349, music.beat(BeatFraction.HALF)),
+        music.PlaybackMode.UNTIL_DONE)
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+def updateOthers():
+    if sensorState != 0:
+        radio.send_number(sensorState)
+        datalogger.log(datalogger.create_cv("event", 1),
+            datalogger.create_cv("signalStrength",
+                abs(radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH))),
+            datalogger.create_cv("sensorState", sensorState),
+            datalogger.create_cv("codeFromRemote", codeFromRemote),
+            datalogger.create_cv("temp", input.temperature()),
+            datalogger.create_cv("accel",
+                max(input.acceleration(Dimension.X),
+                    max(input.acceleration(Dimension.Y),
+                        input.acceleration(Dimension.Z)))))
+signalReceived = 0
+codeFromRemote = 0
+sensorState = 0
+pull1 = 0
+pull0 = 0
+init()
+welcome()
+# sleep / wake-up on event
 
 def on_forever():
-    if input.pin_is_pressed(TouchPin.P0):
-        music.play(music.tone_playable(587, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.show_number(0)
-        basic.clear_screen()
-    if input.pin_is_pressed(TouchPin.P0):
-        music.play(music.tone_playable(220, music.beat(BeatFraction.WHOLE)),
-            music.PlaybackMode.UNTIL_DONE)
-        basic.show_number(1)
-        basic.clear_screen()
+    pass
 basic.forever(on_forever)
+
+# Watch-dog
+
+def on_forever2():
+    pass
+basic.forever(on_forever2)
+
+def on_forever3():
+    readSensors()
+    updateOthers()
+    timeoutVibration()
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,34 +1,572 @@
-input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    basic.showNumber(2)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    basic.clearScreen()
+/**
+ * >>>>>>> remote changes (pulled from Github)
+ */
+/**
+ * input.onButtonPressed(Button.B, function () {
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 1)
+ */
+/**
+ * basic.showNumber(2)
+ */
+/**
+ * =======
+ * 
+ * radio.onReceivedNumber(function (receivedNumber) {
+ * 
+ * updateFromRemote = receivedNumber
+ * 
+ * })
+ * 
+ * // 8G is way too much, but this safety feature needs testing in a real backpack
+ * 
+ * input.onGesture(Gesture.EightG, function () {
+ */
+/**
+ * })
+ * 
+ * // should confirm reception
+ * 
+ * function radioSend (num: number, bool: boolean) {
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 0)
+ */
+/**
+ * basic.clearScreen()
+ */
+/**
+ * })
+ */
+/**
+ * <<<<<<< local changes
+ */
+/**
+ * function FeedbackMyself () {
+ */
+/**
+ * if (sensorState == 3) {
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 1)
+ */
+/**
+ * music.ringTone(165)
+ */
+/**
+ * radio.sendNumber(3)
+ */
+/**
+ * led.plot(0, 0)
+ */
+/**
+ * radio.sendNumber(2)
+ */
+/**
+ * led.plot(0, 1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 0)
+ */
+/**
+ * signalReceived = control.millis()
+ */
+/**
+ * led.plot(0, 0)
+ */
+/**
+ * } else if (sensorState == 1) {
+ */
+/**
+ * led.unplot(0, 1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 1)
+ */
+/**
+ * signalReceived = control.millis()
+ */
+/**
+ * } else if (sensorState == 2) {
+ */
+/**
+ * music.ringTone(262)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 1)
+ */
+/**
+ * music.ringTone(494)
+ */
+/**
+ * radio.sendNumber(1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 0)
+ */
+/**
+ * led.unplot(0, 0)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 0)
+ */
+/**
+ * led.plot(0, 1)
+ */
+/**
+ * signalReceived = control.millis()
+ */
+/**
+ * music.stopAllSounds()
+ */
+/**
+ * led.unplot(0, 0)
+ */
+/**
+ * } else if ((0 as any) == (6 as any)) {
+ */
+/**
+ * led.unplot(0, 1)
+ */
+/**
+ * sensorState = 0
+ */
+/**
+ * radio.sendNumber(6)
+ */
+/**
+ * =======
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 0)
+ */
+/**
+ * function FeedbackMyself () {
+ */
+/**
+ * if (updateLocal == 3) {
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 1)
+ */
+/**
+ * music.ringTone(165)
+ */
+/**
+ * radio.sendNumber(2)
+ */
+/**
+ * radio.sendNumber(3)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 0)
+ */
+/**
+ * led.plot(0, 0)
+ */
+/**
+ * led.plot(0, 1)
+ */
+/**
+ * led.plot(0, 0)
+ */
+/**
+ * } else if (updateLocal == 1) {
+ */
+/**
+ * led.unplot(0, 1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 1)
+ */
+/**
+ * } else if (updateLocal == 2) {
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 1)
+ */
+/**
+ * music.ringTone(262)
+ */
+/**
+ * music.ringTone(494)
+ */
+/**
+ * radio.sendNumber(1)
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 0)
+ */
+/**
+ * led.unplot(0, 0)
+ */
+/**
+ * led.plot(0, 1)
+ */
+/**
+ * led.unplot(0, 0)
+ */
+/**
+ * } else if (updateLocal == 6) {
+ */
+/**
+ * led.unplot(0, 1)
+ */
+/**
+ * radio.sendNumber(6)
+ */
+/**
+ * updateLocal = 0
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P8, 0)
+ */
+/**
+ * }
+ */
+/**
+ * }
+ */
+/**
+ * pins.digitalWritePin(DigitalPin.P12, 0)
+ */
+/**
+ * function updateSensors () {
+ */
+/**
+ * if (input.pinIsPressed(TouchPin.P0)) {
+ */
+/**
+ * music.stopAllSounds()
+ */
+/**
+ * pull0 = 0
+ */
+/**
+ * } else {
+ */
+/**
+ * pull0 = 1
+ */
+/**
+ * }
+ */
+/**
+ * if (input.pinIsPressed(TouchPin.P1)) {
+ */
+/**
+ * pull1 = 0
+ */
+/**
+ * updateLocal = 1
+ */
+/**
+ * } else {
+ */
+/**
+ * } else if (pull0 == 0 && pull1 == 1) {
+ */
+/**
+ * pull1 = 1
+ */
+/**
+ * }
+ */
+/**
+ * updateLocal = 2
+ */
+/**
+ * if (pull0 == 1 && pull1 == 1) {
+ */
+/**
+ * } else {
+ */
+/**
+ * updateLocal = 3
+ */
+/**
+ * if (updateLocal != 0) {
+ */
+/**
+ * } else if (pull0 == 1 && pull1 == 0) {
+ */
+/**
+ * updateLocal = 6
+ */
+/**
+ * }
+ */
+/**
+ * }
+ */
+/**
+ * }
+ */
+/**
+ * let updateLocal = 0
+ */
+/**
+ * =======
+ */
+/**
+ * // Watch-dog
+ */
+/**
+ * basic.forever(function () {
+ */
+/**
+ * })
+ */
+/**
+ * // sleep / wake-up on event
+ */
+/**
+ * basic.forever(function () {
+ */
+/**
+ * })
+ */
+/**
+ * >>>>>>> remote changes (pulled from Github)
+ */
+// <<<<<<< local changes
+function readSensors () {
+    if (input.pinIsPressed(TouchPin.P0)) {
+        pull0 = 0
+    } else {
+        pull0 = 1
+    }
+    if (input.pinIsPressed(TouchPin.P1)) {
+        pull1 = 0
+    } else {
+        pull1 = 1
+    }
+    sensorState = pull0 + pull1 * 2
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    codeFromRemote = receivedNumber
+    updateVibration()
+    record(receivedNumber)
 })
-input.onButtonPressed(Button.B, function () {
+// 8G is way too much, but this safety feature needs testing in a real backpack
+input.onGesture(Gesture.EightG, function () {
+	
+})
+datalogger.onLogFull(function () {
+    record(66)
+    datalogger.deleteLog(datalogger.DeleteType.Fast)
+    record(66)
+})
+function updateVibration () {
+    if (codeFromRemote == 3) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        music.ringTone(165)
+        led.plot(1, 0)
+        led.plot(1, 1)
+        signalReceived = control.millis()
+    } else if (codeFromRemote == 1) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        music.ringTone(262)
+        led.plot(1, 0)
+        led.unplot(1, 1)
+        signalReceived = control.millis()
+    } else if (codeFromRemote == 2) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        music.ringTone(494)
+        led.unplot(1, 0)
+        led.plot(1, 1)
+        signalReceived = control.millis()
+    } else if (codeFromRemote == 9) {
+        linkEstablished = 1
+    }
+}
+// =======
+function updateFromOther () {
+    if (updateFromRemote == 3) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        music.ringTone(165)
+        led.plot(1, 0)
+        led.plot(1, 1)
+    } else if (updateFromRemote == 1) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        music.ringTone(262)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        led.plot(1, 0)
+        led.unplot(1, 1)
+    } else if (updateFromRemote == 2) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        music.ringTone(494)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        led.unplot(1, 0)
+        led.plot(1, 1)
+    } else if (updateFromRemote == 6) {
+        radio.sendNumber(0)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        music.stopAllSounds()
+        led.unplot(1, 0)
+        led.unplot(1, 1)
+        updateFromRemote = 0
+    }
+}
+// }
+// // >>>>>>> remote changes (pulled from Github)
+// input.onButtonPressed(Button.A, function () {
+// pins.digitalWritePin(DigitalPin.P8, 1)
+// basic.showNumber(1)
+// pins.digitalWritePin(DigitalPin.P8, 0)
+// basic.clearScreen()
+// })
+// <<<<<<< local changes
+function record (eventState: number) {
+    datalogger.log(
+    datalogger.createCV("event", eventState),
+    datalogger.createCV("signalStrength", Math.abs(radio.receivedPacket(RadioPacketProperty.SignalStrength))),
+    datalogger.createCV("sensorState", sensorState),
+    datalogger.createCV("codeFromRemote", codeFromRemote),
+    datalogger.createCV("temp", input.temperature()),
+    datalogger.createCV("accel", Math.abs(Math.max(input.acceleration(Dimension.X), Math.max(input.acceleration(Dimension.Y), input.acceleration(Dimension.Z))))),
+    datalogger.createCV("bat", pins.analogReadPin(AnalogReadWritePin.P2))
+    )
+}
+function init () {
+    music.setBuiltInSpeakerEnabled(true)
+    music.setVolume(255)
+    pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+    pull0 = 0
+    pull1 = 0
+    basic.clearScreen()
+    radio.setGroup(1)
+    radio.setTransmitPower(7)
+    radio.setFrequencyBand(0)
+    codeFromRemote = 0
+    sensorState = 0
+    linkEstablished = 0
+    datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
+    serial.redirectToUSB()
+    datalogger.mirrorToSerial(true)
+    datalogger.setColumnTitles(
+    "event",
+    "signalStrength",
+    "sensorState",
+    "codeFromRemote",
+    "temp",
+    "accel",
+    "bat"
+    )
+    record(22)
+}
+// let pull1 = 0
+// let pull0 = 0
+// music.setBuiltInSpeakerEnabled(true)
+// pull0 = 0
+// pull1 = 0
+// music.setVolume(255)
+// music.play(music.stringPlayable("G - B - - F F C5 ", 240), music.PlaybackMode.UntilDone)
+// pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+// pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
+// pins.digitalWritePin(DigitalPin.P8, 1)
+// pins.digitalWritePin(DigitalPin.P12, 1)
+// basic.showIcon(IconNames.Heart)
+// basic.showIcon(IconNames.SmallHeart)
+// pins.digitalWritePin(DigitalPin.P8, 0)
+// pins.digitalWritePin(DigitalPin.P12, 0)
+// basic.clearScreen()
+// radio.setGroup(1)
+// radio.setFrequencyBand(0)
+// updateFromRemote = 0
+// updateLocal = 0
+// basic.forever(function () {
+// updateSensors()
+// if (updateFromRemote == 0) {
+// FeedbackMyself()
+// } else {
+// updateFromOther()
+// // >>>>>>> remote changes (pulled from Github)
+// // }
+// // <<<<<<< local changes
+// }
+function timeoutVibration () {
+    if (control.millis() - signalReceived >= 100) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        music.stopAllSounds()
+        led.unplot(1, 0)
+        led.unplot(1, 1)
+        codeFromRemote = 0
+    }
+}
+function welcome () {
     pins.digitalWritePin(DigitalPin.P12, 1)
-    basic.showNumber(2)
+    music.play(music.tonePlayable(131, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
     pins.digitalWritePin(DigitalPin.P12, 0)
-    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    music.play(music.tonePlayable(523, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    music.play(music.tonePlayable(349, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+function updateOthers () {
+    if (sensorState != 0) {
+        radio.sendNumber(sensorState)
+        record(sensorState)
+    }
+}
+let updateFromRemote = 0
+let linkEstablished = 0
+let signalReceived = 0
+let codeFromRemote = 0
+let sensorState = 0
+let pull1 = 0
+let pull0 = 0
+init()
+welcome()
+// <<<<<<< local changes
+// watch-dog - sends a code 9 every 5 seconds to the other unit
+loops.everyInterval(1000, function () {
+    radio.sendNumber(9)
 })
-music.play(music.stringPlayable("G - B - - F F C5 ", 240), music.PlaybackMode.UntilDone)
-pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
-pins.digitalWritePin(DigitalPin.P8, 1)
-pins.digitalWritePin(DigitalPin.P12, 1)
-basic.showIcon(IconNames.Heart)
-basic.showIcon(IconNames.SmallHeart)
-pins.digitalWritePin(DigitalPin.P8, 0)
-pins.digitalWritePin(DigitalPin.P12, 0)
-basic.clearScreen()
+// =======
+// >>>>>>> remote changes (pulled from Github)
 basic.forever(function () {
-    if (!(input.pinIsPressed(TouchPin.P0))) {
-        music.play(music.tonePlayable(587, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        basic.showNumber(0)
-        basic.clearScreen()
+    readSensors()
+    updateOthers()
+    timeoutVibration()
+})
+// <<<<<<< local changes
+// watch-dog - sends a code 9 every 5 seconds to the other unit
+loops.everyInterval(5000, function () {
+    if (linkEstablished == 0) {
+        signalReceived = control.millis() + 1000
+        record(99)
+        for (let index = 0; index < 4; index++) {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+            led.plot(5, 5)
+            pins.digitalWritePin(DigitalPin.P8, 1)
+            basic.pause(50)
+            pins.digitalWritePin(DigitalPin.P8, 0)
+            pins.digitalWritePin(DigitalPin.P12, 1)
+            basic.pause(50)
+            pins.digitalWritePin(DigitalPin.P12, 0)
+            led.unplot(5, 5)
+        }
     }
-    if (!(input.pinIsPressed(TouchPin.P1))) {
-        music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-        basic.showNumber(1)
-        basic.clearScreen()
-    }
+    linkEstablished = 0
 })
